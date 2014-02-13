d2ebe4afb50dfeeca53e2f220e46b110557f2b9e
Lloyd <lloydmeta@gmail.com>
2014-02-13 18:48:13 +0900

    Dry up handling of stuff

--- src/main/scala/com/beachape/metascraper/scalatra/MetascraperScalatraServlet.scala
+++ src/main/scala/com/beachape/metascraper/scalatra/MetascraperScalatraServlet.scala
@@ -7,7 +7,12 @@ import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
 import com.beachape.metascraper.Messages.ScrapedData
 import scala.concurrent.ExecutionContext
+import javax.servlet.http.HttpServletRequest
 
+/**
+ * Case class for extracting URL from JSON params
+ * @param url String
+ */
 case class ScrapeRequest(url: String)
 
 class MetascraperScalatraServlet(val scraper: MetadataScraper)(implicit val executor: ExecutionContext)
@@ -16,6 +21,7 @@ class MetascraperScalatraServlet(val scraper: MetadataScraper)(implicit val exec
   with FutureSupport {
 
   protected implicit val jsonFormats: Formats = DefaultFormats.withBigDecimal
+
 
   get("/") {
     <html>
@@ -26,22 +32,24 @@ class MetascraperScalatraServlet(val scraper: MetadataScraper)(implicit val exec
   }
 
   post("/scrape") {
+    val url = urlPostParam(request)
     new AsyncResult {
       contentType = formats("json")
-      val is = scraper.scrape(params.getOrElse("url", "notProvided")) map {
+      val is = scraper.scrape(url) map {
         case Right(data) => data
         case Left(err) => Map("error" -> err.getMessage)
       }
     }
   }
 
-  post("/scrape", request.contentType == Some(formats("json"))) {
-    new AsyncResult {
-      contentType = formats("json")
-      val is = scraper.scrape(parsedBody.extract[ScrapeRequest].url) map {
-        case Right(data) => data
-        case Left(err) => Map("error" -> err.getMessage)
+  def urlPostParam (req: HttpServletRequest): String = {
+    val notPovidedUrl = "notProvided"
+    req.contentType match {
+      case Some(cType) if cType == formats("json") => {
+        try { parsedBody.extract[ScrapeRequest].url }
+        catch { case _ => notPovidedUrl }
       }
+      case _ => params.getOrElse("url", notPovidedUrl)
     }
   }
 
